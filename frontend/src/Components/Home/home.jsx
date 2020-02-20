fc211e90c9d26a48441c6acef6df58aa3fb46071
pablofdm <pablofernandes72.pf@gmail.com>
2020-02-20 00:17:49 -0300

    Primeiro commit

---
+++ frontend/src/Components/Home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../Templates/Main'
+
+
+export default props =>
+        <Main icon="home" title="inicio" subtitle="Segundo Projeto React.">
+                <div className='display-4'>Bem-vindo!</div>
+                <hr />
+                <p className="mb-0">Entre no aplicativo e encontre sua metade da laranja por ai! <i class="fa fa-bolt"></i></p>
+        </Main>
